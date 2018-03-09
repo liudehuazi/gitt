c533532b19837d727aa88e00e8849e3dd6480c97
yuanlu <13650674751@qq.com>
2018-03-09 20:50:14 +0800

    my

---
+++ src/main/java/User.java
@@ -0,0 +1,14 @@
+public class User {
+    private  int id;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
